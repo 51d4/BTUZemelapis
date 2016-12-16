ed536baeeedb80c5f63b372b221dddf1c743172e
Sida <s.nakrosyte@gmail.com>
2016-12-16 15:37:17 +0100

    First commit

---
+++ BTUZemelapis/BTUZemelapis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTUZemelapis
+//
+//  Created by MacBook on 16/12/16.
+//  Copyright © 2016 Betliejaus Taikos Ugnis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
